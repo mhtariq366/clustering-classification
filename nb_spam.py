9976ac298029f09a8f0e79e67833dd277a7a5433
Hassan <mhtariq366@gmail.com>
2025-08-09 23:40:20 +0500

    Update nb_spam.py

--- nb_spam.py
+++ nb_spam.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
+# reading dataset from csv file
 df = pd.read_csv("datasets/spam.csv", encoding="latin-1")
+
+# drop columns 2, 3 and 4.
 df = df.drop(columns=["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"])
+
+# renaming the text and given label columns
 df = df.rename(columns={"v1":"Label", "v2":"Text"})
 
